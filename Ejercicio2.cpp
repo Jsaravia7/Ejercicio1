71424abecbde7999e295b1d8f4720f9a013a72d4
Jorge Saravia <jorgesaravia882@gmail.com>
2022-06-07 16:21:46 -0600

    Trabajo 2

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -15,10 +15,47 @@ void cargarLibros(){
 int main(int argc, char const *argv[])
 {
     cargarLibros();
-    
-    for (int i = 0; i < 2; i++)
+     
+    bool salir = false;
+
+    while (salir == false)
     {
-        cout<<libros[i][1]<<endl;
-    }
-    return 0;
+        string buscar = "";
+        system("cls");
+        cout<<"Ingrese la descripcion del libro que busca: ";
+        cin>>buscar;
+        //busqueda
+        for (int i = 0; i < 2; i++)
+        {
+            string libro = libros [i][1];
+            if (libro.find(buscar) != string::npos )
+            {
+                cout<<"Libro encontrado: "<< libro <<endl;
+                salir = true;
+            }
+            
+        }
+        
+        if (salir == false)
+        {
+            char continuar = 'n';
+        while (true)
+        {
+          system("cls");
+          cout<<"No se encontro el libro que busca. Desea continuar? s/n: ";
+          cin>>continuar;
+
+          if (continuar == 's' || continuar == 'S')
+          {
+            break;
+          }else if (continuar == 'n' || continuar == 'N')
+          {
+              salir = true;
+              break;
+          }         
+        }
+      }
+        
+   } 
+    return 0;  
 }
