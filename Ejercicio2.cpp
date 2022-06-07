735c047fecf05bfb3410b8a407fd63feec0cdb34
Jorge Saravia <jorgesaravia882@gmail.com>
2022-06-07 15:56:50 -0600

    Trabajo2

--- Ejercicio2.cpp
+++ Ejercicio2.cpp
@@ -5,7 +5,7 @@ string libros[2][2];
 
 void cargarLibros(){
    libros[0][0] = "Algoritmos";
-   libros[0][1] = "Algoritmos y Programacion (Guia de docentes";
+   libros[0][1] = "Algoritmos y Programacion (Guia de docentes)";
 
    libros[1][0] = "Base de datos";
    libros[1][1] = "Apuntes de Base de Datos 1";
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[])
     
     for (int i = 0; i < 2; i++)
     {
-        cout<<libros[i][1];
+        cout<<libros[i][1]<<endl;
     }
     return 0;
 }
