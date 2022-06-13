09d0a6176b625f0a9dcace1ca005ac6d9cf34180
Jorge Saravia <jorgesaravia882@gmail.com>
2022-06-13 14:00:10 -0600

    Proyecto

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -8,13 +8,25 @@ int main(int argc, char const *argv[])
 {
     system("cls");
 
+    int eleccion;
+
     cout<<"**************"<<endl;
-    cout<<"Menu de Juegos";
+    cout<<"Menu de Juegos"<<endl;
     cout<<"**************"<<endl;
 
-    // starShip();
-    // snake();
+    cout<<"Seleccione un juego"<<endl<<endl;
+    cout<<"1 - StarShip "<<endl<<endl;
+    cout<<"2 - Snake "<<endl<<endl;
 
+    cout<<"Respuesta: ";
+    cin>>eleccion;
+    
+     if (eleccion == 1)
+     {
+        system("cls");
+        starShip();
+     }else 
+        system("cls");
+        snake();
     return 0;
 }
-
