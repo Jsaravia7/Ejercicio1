154088c5daab73e1822fccf5f093703bd0719690
Jorge Saravia <jorgesaravia882@gmail.com>
2022-06-13 13:22:08 -0600

    Proyecto

---
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    cout<<"**************"<<endl;
+    cout<<"Menu de Juegos";
+    cout<<"**************"<<endl;
+
+    // starShip();
+    // snake();
+
+    return 0;
+}
